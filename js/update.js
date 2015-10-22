c11939f000ecb2489d57d94e7ddf335fd25f8ad6
KASIYOO <he19911992@gmail.com>
2015-10-22 17:00:55 +0900

    test

--- js/update.js
+++ js/update.js
@@ -1,1 +1,3 @@
 document.getElementById("update").style.display="block";
+document.getElementById("warning").innerHTML( "Your browser is too old to handle this website. <br> Please upgrade to a modern browser:");
+
